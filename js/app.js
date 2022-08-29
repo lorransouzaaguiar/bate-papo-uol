bed8f26fac849f1939d9d50ff6112c230d1ca287
Lorran Aguiar <lorransouzaaguiar2010@gmail.com>
2022-08-29 20:06:29 -0300

    feat: adiciona criação de usuário ao entrar na sala

--- js/app.js
+++ js/app.js
@@ -1,32 +1,36 @@
 console.log('ola mundo')
+const URLBASE = 'https://mock-api.driven.com.br/api/v6/uol'
 
-const getMessages = async () => {
-    const response = await fetch('https://mock-api.driven.com.br/api/v6/uol/messages')
-    const data = await response.json()
-    return data
+const repo = () => {
+    const getMessages = async () => {
+        const response = await axios.get(`${URLBASE}/messages`)
+        return response.data
+    }
+
+    const createUser = async (name) => {
+        const response = await axios.post(`${URLBASE}/participants`, {
+            name
+        })
+        return response.data
+    }
+
+    return { getMessages, createUser }
 }
 
 
-/* const createUser = async (name) => {
-    const response = await fetch('https://mock-api.driven.com.br/api/v6/uol/participants', {
-        method: 'post',
-        body: {
-            'name': name
-        }
-    })
-    const data = await response.json()
-    console.log(data)
-    return data
-} */
+const getColorByTypeValue = (type) => ({
+    'status': 'var(--g1)',
+    'message': 'var(--w1)',
+    'private_message': 'var(--r1)',
+})[type] || 'var(--w1)'
 
-/* createUser('Lorran').then(console.log) */
+
 
 const renderMessages = async () => {
     const messageList = document.createElement('ul')
-    const msgs = await getMessages()
-    console.log(msgs)
+    const msgs = await repo().getMessages()
 
-    msgs.forEach((msg, index) => {
+    msgs.forEach((msg) => {
         const msgEl = document.createElement('li')
         msgEl.classList.add('message')
 
@@ -34,34 +38,27 @@ const renderMessages = async () => {
             <span class="time">(${msg.time})</span>
             <p><strong>${msg.from}</strong> para <strong>${msg.to}</strong> ${msg.text}</p>
         `
-
-        if (msg.type === 'status') {
-            msgEl.style.backgroundColor = 'var(--g1)'
-        }
-
-        if (msg.type === 'message') {
-            msgEl.style.backgroundColor = 'var(--w1)'
-
-        }
-
-        if (msg.type === 'private_message') {
-            msgEl.style.backgroundColor = 'var(--r1)'
-
-        }
-
-
+        msgEl.style.backgroundColor = getColorByTypeValue(msg.type)
 
         messageList.appendChild(msgEl)
 
     })
-    console.log(messageList.childNodes.length)
     return messageList
 }
 
-const main = document.getElementById('main')
+(async function App() {
+    const main = document.getElementById('main')
 
-renderMessages().then(listEl => {
-    main.appendChild(listEl)
+    const inputName = prompt('Insira seu lindo nome!')
 
-    console.log(document.querySelector('ul'))
-}).catch(console.log)
+    await repo().createUser(inputName)
+
+
+    renderMessages().then(listEl => {
+        main.appendChild(listEl)
+
+        console.log(document.querySelector('ul'))
+    }).catch(console.log)
+
+})()
+
